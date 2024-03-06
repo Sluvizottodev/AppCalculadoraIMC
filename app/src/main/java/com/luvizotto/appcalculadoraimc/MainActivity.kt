de64cfe95a0820fa78efca954754fdd5fe5d7d39
StefaniLuvizotto <luvizottodev@gmail.com>
2024-03-06 15:00:12 -0300

    Interface commit

--- app/src/main/java/com/luvizotto/appcalculadoraimc/MainActivity.kt
+++ app/src/main/java/com/luvizotto/appcalculadoraimc/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.luvizotto.appcalculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Código para Activity 2
+        btnCalcular = findViewById(R.id.btnCalcular)
+
+        btnCalcular.setOnClickListener{
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            startActivity( intent )
+        }
+
+
     }
 }
